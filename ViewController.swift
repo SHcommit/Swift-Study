59b330f811964e3e357a14b37ead6acabc23eb9e
YSH <happysh_s2@naver.com>
2022-01-08 23:03:11 +0900

    Create ViewController.swift 	Date : 20.01.08

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by 양승현 on 2022/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
