5f3ddf3f697cff16684344c08d1de9a37b481dff
YSH <happysh_s2@naver.com>
2022-01-09 22:56:08 +0900

    Add further explanation 	Ios ViewController - ViewController class 	Date : 20.01.09

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+/*
+                중간 정리(22.01.09)
+    - View Controller에서 클라이언트 영역에 컨트롤들을 배치한다.
+    - 위에서 배치한 컨트롤에 대한 View Controller 클래스에
+        해당 레이블 ,버튼 등을 드래그 한다.
+    -이에 대한 정보는 우측 Triggered Segues에
+        화면과 클래스 간 멤버 변수화, 현재 연결된 정보, 화면에는 없지만
+        클래스에만 존재하는 잘못 연결된 정보들을 알 수 있다.
+ */
 
 }
 
