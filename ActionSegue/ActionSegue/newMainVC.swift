b5d4f41478e12b1588d873cca56586ed40b7690b
YSH <happysh_s2@naver.com>
2022-01-14 00:07:54 +0900

    Create newMainVC 	Date 20.01.14

---
+++ ActionSegue/ActionSegue/newMainVC.swift
@@ -0,0 +1,14 @@
+//
+//  newMainVC.swift
+//  ActionSegue
+//
+//  Created by 양승현 on 2022/01/14.
+//
+
+import Foundation
+import UIKit
+
+class newMainViewController : UIViewController{
+    
+    
+}
