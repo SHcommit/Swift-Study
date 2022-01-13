e30a36b1c208b5e792000fe9f4b641a573f0f3e6
YSH <happysh_s2@naver.com>
2022-01-14 00:27:07 +0900

    Add Unwind method 	Date : 20.01.14

--- ActionSegue/ActionSegue/newMainVC.swift
+++ ActionSegue/ActionSegue/newMainVC.swift
@@ -3,12 +3,17 @@
 //  ActionSegue
 //
 //  Created by 양승현 on 2022/01/14.
-//
+//  디폴트 ViewController class 는 액션 세그웨이에 관한 것이어서
+// 추가적으로 새로운 ViewController class 를 구현했다.
 
 import Foundation
 import UIKit
 
 class newMainViewController : UIViewController{
     
+    // Unwind 메서드.
+    @IBAction func gotoPage1(_ sender : UIStoryboardSegue){
+        
+    }
     
 }
