7bb041e77cd0cc2110a8239e0f15eeaa65878e97
YSH <happysh_s2@naver.com>
2022-01-16 21:33:37 +0900

    Create SubmitValue - back 	Date : 20.01.16

---
+++ SubmitValue_back/SubmitValue_back/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmitValue_back
+//
+//  Created by 양승현 on 2022/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
