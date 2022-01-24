d5bd64f412be54ef8fde3407b88b7e014d20da79
YSH <happysh_s2@naver.com>
2022-01-24 15:39:04 +0900

    Add UIImagePickerController()

--- ImagePicherDelegate/ImagePicherDelegate/ViewController.swift
+++ ImagePicherDelegate/ImagePicherDelegate/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet var imgView: UIImageView!;
+    @IBAction func imgPick(_ sender: Any){
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary;
+        picker.allowsEditing = true;
+        
+        self.present(picker, animated: false);
+    }
 
 }
 
