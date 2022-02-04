8269381849129d1ba0b122c9952c2e08315ba40e
ysh <happysh_s2@naver.com>
2022-02-04 14:55:59 +0900

    Create MovieVO

---
+++ TableViewProject/tableViewProj2/tableViewProj2/MovieIO.swift
@@ -0,0 +1,24 @@
+//
+//  MovieIO.swift
+//  tableViewProj2
+//
+//  Created by 양승현 on 2022/02/04.
+//
+
+import Foundation
+/**
+ *섬네일
+ *제목
+ *설명
+ *상세 정보
+ *개봉일
+ *평점
+ */
+class MovieIO{
+    var thumbnail: String?
+    var title: String?
+    var description: String?
+    var detail : String?
+    var opendate: String?
+    var rating: Double?
+}
