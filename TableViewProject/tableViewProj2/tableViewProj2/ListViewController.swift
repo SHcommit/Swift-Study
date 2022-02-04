ed1a8e08d0e449d3da8f560d556c9fc9b52a7a3d
ysh <happysh_s2@naver.com>
2022-02-04 14:57:41 +0900

    Create ListViewController.swift

---
+++ TableViewProject/tableViewProj2/tableViewProj2/ListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewController.swift
+//  tableViewProj2
+//
+//  Created by 양승현 on 2022/02/04.
+//
+
+import Foundation
+import UIKit
+
+class ListViewController : UITableViewController{
+    
+}
