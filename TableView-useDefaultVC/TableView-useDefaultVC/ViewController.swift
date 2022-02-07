def112f8ff70249e5f1ca914c8bfe752f82d135c
ysh <happysh_s2@naver.com>
2022-02-07 15:58:52 +0900

    Create tableView in DefaultViewController

---
+++ TableView-useDefaultVC/TableView-useDefaultVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView-useDefaultVC
+//
+//  Created by 양승현 on 2022/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
