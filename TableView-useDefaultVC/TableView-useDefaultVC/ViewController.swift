64bf7daeb09ea7ac985ba677899d2c3e4c8e2934
ysh <happysh_s2@naver.com>
2022-02-07 16:18:39 +0900

    Implement tableViewController prototype in default VC

--- TableView-useDefaultVC/TableView-useDefaultVC/ViewController.swift
+++ TableView-useDefaultVC/TableView-useDefaultVC/ViewController.swift
@@ -16,4 +16,15 @@ class ViewController: UIViewController {
 
 
 }
+/**
+ *인터페이스 빌더에서 기본 VC(ViewController)를 사용하는 경우,
+ *tableViewController를 사용할때의 클래스에서 디폴트로 추가된 UITableViewDataSource, UITableViewDelegate
+ *두 프로토콜을 추가해해야 한다.*
+ */
+extension ViewController: UITableViewDataSource{
+    
+}
+extension ViewController: UITableViewDelegate{
+    
+}
 
