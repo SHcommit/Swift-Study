96429ef069d434e40cafe0269523c07f570fe840
ysh <happysh_s2@naver.com>
2022-03-08 23:31:52 +0900

    Override perform()

--- Ch4_화면전환/Scene-CustomSegue/Scene-CustomSegue/NewSegue.swift
+++ Ch4_화면전환/Scene-CustomSegue/Scene-CustomSegue/NewSegue.swift
@@ -5,4 +5,20 @@
 //  Created by 양승현 on 2022/03/08.
 //
 
-import Foundation
+import UIKit
+
+class NewSegue : UIStoryboardSegue {
+    
+    //prepare(for : sender:) 메서드는 Segue가 실행되기 전에 실행되는 메서드이다.
+    
+    override func perform(){
+        
+        //세그웨이 출발지 뷰 컨트롤러
+        let srcUVC = self.source
+        
+        //세그웨이 도착지 VC
+        let destUVC = self.destination
+        
+        UIView.transition(from: srcUVC.view, to: destUVC.view, duration: 2, options: .transitionCurlDown)
+    }
+}
