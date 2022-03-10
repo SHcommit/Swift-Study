45ed57c70690fd2397e1cbfb2c40338a58643de0
ysh <happysh_s2@naver.com>
2022-03-10 14:56:11 +0900

    Add label, Outlet property, func. and custom Layout

--- Ch5_submitValue/Ch5_SubmitValue/Ch5_SubmitValue/ViewController.swift
+++ Ch5_submitValue/Ch5_SubmitValue/Ch5_SubmitValue/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
-
+/**
+ *현재 VC의 목적
+ *아래의 세개 아울렛 변수
+ * tfEmail, isUpdateSwitch, intervalStepper과 연결된 콘텐츠에
+ *사용자가 입력한 값을 받아서, 다음 화면SecondVC에서 그 값을 전달 후 화면에 출력할 것이다. *
+ *
+ */
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet var tfEmail: UITextField!
+    
+    @IBOutlet var isUpdateSwitch: UISwitch!
+    
+    @IBOutlet var isUpdateText: UILabel!
+    
+    @IBAction func onSwitch(_ sender: Any) {
+        if isUpdateSwitch.isOn {
+            isUpdateText.text = "자동갱신"
+        }else{
+            isUpdateText.text = "자동갱신 안함"
+        }
+    }
+    
+    @IBOutlet var intervalStepper: UIStepper!
+    
+    @IBOutlet var intervalText: UILabel!
+    
+    @IBAction func onStepper(_ sender: Any) {
+        let value = Int((sender as! UIStepper).value)
+        self.intervalText.text = "\(value) 분 마다"
+    }
+    
+    
 }
 
