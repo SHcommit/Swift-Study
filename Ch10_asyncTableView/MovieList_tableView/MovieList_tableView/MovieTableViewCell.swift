f8fa5bdfdb527991a2365b7a5d4ca194ac02ff21
ysh <happysh_S2@naver.com>
2022-04-26 23:58:25 +0900

    Add Movie list in tableView without looking at the book :)

--- Ch10_asyncTableView/MovieList_tableView/MovieList_tableView/MovieTableViewCell.swift
+++ Ch10_asyncTableView/MovieList_tableView/MovieList_tableView/MovieTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  MovieTableCell.swift
+//  MovieTableViewCell.swift
 //  MovieList_tableView
 //
 //  Created by 양승현 on 2022/04/26.
@@ -8,6 +8,15 @@
 import UIKit
 
 class MovieTableViewCell : UITableViewCell{
+    
+    @IBOutlet var thumbnail: UIImageView!
+    
+    @IBOutlet var rating: UILabel!
+    
+    @IBOutlet var title: UILabel!
+    
+    //영화 상세 묘사
+    @IBOutlet var desc: UILabel!
     
 }
 
--- Ch10_asyncTableView/MovieList_tableView/MovieList_tableView/MovieVO.swift
+++ Ch10_asyncTableView/MovieList_tableView/MovieList_tableView/MovieVO.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class MovieVO {
+    /**
+     * 섬네일 이미지 주소
+     * 제목
+     * 설명
+     * 상세정보
+     * 평점*
+     */
+    var thumbnail :String?
+    var title: String?
+    var description: String?
+    var detail : String?
+    var rating : Double?
+}
