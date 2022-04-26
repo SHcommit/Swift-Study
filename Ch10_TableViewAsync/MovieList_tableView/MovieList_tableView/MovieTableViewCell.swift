9ae6fbbcf02d917d17087b8bfb88a85b1e5134b5
ysh <happysh_S2@naver.com>
2022-04-27 01:27:13 +0900

    Add async tableView in ImageView(thumbnail)

---
+++ Ch10_TableViewAsync/MovieList_tableView/MovieList_tableView/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MovieList_tableView
+//
+//  Created by 양승현 on 2022/04/26.
+//
+
+import UIKit
+
+class MovieTableViewCell : UITableViewCell{
+    
+    @IBOutlet var thumbnail: UIImageView!
+    
+    @IBOutlet var rating: UILabel!
+    
+    @IBOutlet var title: UILabel!
+    
+    //영화 상세 묘사
+    @IBOutlet var desc: UILabel!
+    
+    
+}
+
---
+++ Ch10_TableViewAsync/MovieList_tableView/MovieList_tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList_tableView
+//
+//  Created by 양승현 on 2022/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
