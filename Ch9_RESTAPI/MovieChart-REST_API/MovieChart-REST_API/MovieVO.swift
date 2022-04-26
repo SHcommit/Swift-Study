efc161dd49eac3c42b9e9dc7701fa08d42446b6b
ysh <happysh_S2@naver.com>
2022-04-26 13:43:55 +0900

    Add MovieVO class

--- Ch9_RESTAPI/MovieChart-REST_API/MovieChart-REST_API/MovieVO.swift
+++ Ch9_RESTAPI/MovieChart-REST_API/MovieChart-REST_API/MovieVO.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class MovieVO {
+    /**
+     * 섬네일 이미지 주소
+     * 제목
+     * 설명
+     * 상세정보
+     * 개봉일
+     * 평점*
+     */
+    var thumbnail :String?
+    var title: String?
+    var description: String?
+    var detail : String?
+    var opendate : String?
+    var rating : Double?
+}
