fd36954f86dacb2ff753fec7b0d2019553421c24
ysh <happysh_S2@naver.com>
2022-04-28 01:18:29 +0900

    Add MovieVO. RESTAPI data save in MovieVO variables

--- Ch11_WebView/MovieChart_webView/MovieChart_webView/MovieVO.swift
+++ Ch11_WebView/MovieChart_webView/MovieChart_webView/MovieVO.swift
@@ -5,4 +5,18 @@
 //  Created by 양승현 on 2022/04/28.
 //
 
-import Foundation
+import UIKit
+
+//설명은
+//https://github.com/SHcommit/SwiftStudy_No2/blob/master/Ch10_TableViewAsync/MovieList_tableView/MovieList_tableView/MovieVO.swift
+
+class MovieVO {
+    
+    var thumbnail :String?
+    var title: String?
+    var description: String?
+    var detail : String?
+    var rating : Double?
+    
+    var thumbnaliImage : UIImage?
+}
