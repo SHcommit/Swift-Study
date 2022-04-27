3ae26703b3896d56adc2834f5a75a29277130780
ysh <happysh_S2@naver.com>
2022-04-28 01:18:06 +0900

    Create file

---
+++ Ch11_WebView/MovieChart_webView/MovieChart_webView/CustomTabieViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CustomTabieViewCell.swift
+//  MovieChart_webView
+//
+//  Created by 양승현 on 2022/04/28.
+//
+
+import Foundation
---
+++ Ch11_WebView/MovieChart_webView/MovieChart_webView/MovieVO.swift
@@ -0,0 +1,8 @@
+//
+//  MovieVO.swift
+//  MovieChart_webView
+//
+//  Created by 양승현 on 2022/04/28.
+//
+
+import Foundation
