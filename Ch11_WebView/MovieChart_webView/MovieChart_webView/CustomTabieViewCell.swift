5f7357482e1ae85c9a039e3b465ae7a2bcf430f2
ysh <happysh_S2@naver.com>
2022-04-28 01:51:41 +0900

    Add Movie list in tableView :)

--- Ch11_WebView/MovieChart_webView/MovieChart_webView/CustomTabieViewCell.swift
+++ Ch11_WebView/MovieChart_webView/MovieChart_webView/CustomTabieViewCell.swift
@@ -5,4 +5,16 @@
 //  Created by 양승현 on 2022/04/28.
 //
 
-import Foundation
+import UIKit
+
+class CustomTabieViewCell : UITableViewCell{
+    
+    @IBOutlet var thumbnail: UIImageView!
+    
+    @IBOutlet var title: UILabel!
+    
+    @IBOutlet var desc: UILabel!
+    
+    @IBOutlet var rating: UILabel!
+    
+}
