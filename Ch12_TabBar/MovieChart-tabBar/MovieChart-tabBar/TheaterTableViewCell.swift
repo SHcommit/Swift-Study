8655d565c11718756fac949127db418d17533c30
ysh <happysh_S2@naver.com>
2022-04-28 19:52:21 +0900

    Add @IBoutlet variables

--- Ch12_TabBar/MovieChart-tabBar/MovieChart-tabBar/TheaterTableViewCell.swift
+++ Ch12_TabBar/MovieChart-tabBar/MovieChart-tabBar/TheaterTableViewCell.swift
@@ -5,4 +5,13 @@
 //  Created by 양승현 on 2022/04/28.
 //
 
-import Foundation
+import UIKit
+
+class TheaterTableViewCell : UITableViewCell{
+    
+    @IBOutlet var name: UILabel!
+    
+    @IBOutlet var addr: UILabel!
+    
+    @IBOutlet var tel: UILabel!
+}
