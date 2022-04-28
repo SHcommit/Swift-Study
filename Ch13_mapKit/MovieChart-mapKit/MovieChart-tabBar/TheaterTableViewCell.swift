18af53c0c60a6b159eace0d2a8902b627ae14b6f
Yang seung hyun <96910404+SHcommit@users.noreply.github.com>
2022-04-28 22:19:52 +0900

    Add files via upload

---
+++ Ch13_mapKit/MovieChart-mapKit/MovieChart-tabBar/TheaterTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TheaterTableViewCell.swift
+//  MovieChart-tabBar
+//
+//  Created by 양승현 on 2022/04/28.
+//
+
+import UIKit
+
+class TheaterTableViewCell : UITableViewCell{
+    
+    @IBOutlet var name: UILabel!
+    
+    @IBOutlet var addr: UILabel!
+    
+    @IBOutlet var tel: UILabel!
+}
