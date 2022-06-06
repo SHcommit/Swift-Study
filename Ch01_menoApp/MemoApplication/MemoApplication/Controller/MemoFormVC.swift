580298f7df9da1437c1ab966d1c115eda27e38eb
ysh <happysh_s2@naver.com>
2022-06-06 13:54:02 +0900

    Add variables, eventHandler

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/MemoFormVC.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/MemoFormVC.swift
@@ -7,8 +7,41 @@
 
 import UIKit
 
-class MemoFormVC: UIViewController {
-
+class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - variable
+    /**
+     * @param subjecet : 제목 저장
+     * @Outlet contents : 사용자 글 작성 tevtView 객체
+     * @Outlet preview  : 이미지 미리보기
+     */
+    var subject : String!
+    @IBOutlet weak var contents: UITextView!
+    @IBOutlet weak var preview: UIImageView!
+    
+    // MARK: - eventHandler
+    
+    // 저장 버튼 클릭시 호출 메서드
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    /**
+     * 카메라 버튼 클릭시 호출 메서드
+     * Todo :
+     *  1. 이미지 피커 인스턴스 생성
+     *  2. 인스턴스의 delegate를 현재 VC로 설정
+     *  3. .allowsEditing 이미지 편집 허용
+     *  4. 화면 전환
+     */
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        self.present(picker,animated: false)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
