8ac0476d0a8deda1345b5803affeddce4c486ca8
ysh <happysh_s2@naver.com>
2022-06-06 13:53:14 +0900

    Create file

---
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  MemoApplication
+//
+//  Created by 양승현 on 2022/06/06.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Ch01_menoApp/MemoApplication/MemoApplication/Model/MemoData.swift
@@ -0,0 +1,8 @@
+//
+//  MemoData.swift
+//  MemoApplication
+//
+//  Created by 양승현 on 2022/06/06.
+//
+
+import Foundation
