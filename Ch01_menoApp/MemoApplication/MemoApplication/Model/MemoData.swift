ed1b318f3395ff348e73f0666e4cccb293d683ba
ysh <happysh_s2@naver.com>
2022-06-06 13:54:24 +0900

    Add memoData model :)

--- Ch01_menoApp/MemoApplication/MemoApplication/Model/MemoData.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Model/MemoData.swift
@@ -5,4 +5,19 @@
 //  Created by 양승현 on 2022/06/06.
 //
 
-import Foundation
+import UIKit
+
+class MemoData {
+    /**
+     * @param memoIndex : 데이터 식별값
+     * @param title              : 메모 제목
+     * @param contents      : 메모 내용
+     * @param image          : 이미지
+     * @param regdate        : 작성일
+     */
+    var memoIndex : Int?
+    var title     : String?
+    var contents  : String?
+    var image     : UIImage?
+    var regdate   : Date?
+}
