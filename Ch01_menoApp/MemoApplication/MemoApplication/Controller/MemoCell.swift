ec2150c330c1fcb03be96bcff353abbee396310d
ysh <happysh_s2@naver.com>
2022-06-08 20:15:24 +0900

    Add Code review

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/MemoCell.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/MemoCell.swift
@@ -6,18 +6,17 @@
 //
 
 import UIKit
-
+/**
+ * TODO :
+ *   인터페이스에서 두 가지의 추가 기능이 있다.
+ *   첫번째는 image 사용하지 않은 버전, 두번째는 사용한 버전.
+ *   이에 따라서 image를 input 하지 않을 경우 nil 을 활용한다.
+ *   Cell 인스턴스가 담긴 클래스다.
+ */
 class MemoCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var subject: UILabel!
+    @IBOutlet weak var contents : UILabel!
+    @IBOutlet weak var regdate  : UILabel!
+    @IBOutlet weak var img: UIImageView!
+    
 }
