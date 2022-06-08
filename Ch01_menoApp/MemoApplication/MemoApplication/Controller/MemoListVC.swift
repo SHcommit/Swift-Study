bdac1b643e0257d41432c4b9a66b6a60eccba833
ysh <happysh_s2@naver.com>
2022-06-08 20:11:02 +0900

    Fix dateFormatter

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/MemoListVC.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/MemoListVC.swift
@@ -8,38 +8,68 @@
 import UIKit
 
 class MemoListVC: UITableViewController {
-
+    var appDelegate : AppDelegate {
+        UIApplication.shared.delegate as! AppDelegate
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillAppear(_ animated : Bool){
+        self.tableView.reloadData()
+    }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    /**
+     * 테이블 뷰 cell 몇개로 할건지?
+     * TODO : AppDelegate의 멤버변수 가져와서 count 반환
+     */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return appDelegate.memoList.count
     }
 
-    /*
+    /**
+     * 테이블 셀 레이블에 데이터 지정
+     * TODO :
+     *  1. 재사용 큐에서 특정 cell 인스턴스 가져옴
+     *  2. AppDelelgate에 있는 memoList에서 데이터를 cell의 레이블에 삽입
+     *  3. 데이터 받은 cell 반환
+     */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
         // Configure the cell...
-
+        let rowData = self.appDelegate.memoList[indexPath.row]
+        let cellId  = rowData.image == nil ? "memoCell" : "memoCellWithImage"
+        let cell    = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
+        cell.subject?.text  = rowData.title
+        cell.contents?.text = rowData.contents
+        cell.img?.image     = rowData.image
+        
+        let formatter       = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text  = formatter.string(from: rowData.regdate!)
         return cell
     }
-    */
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    /*
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if appDelegate != nil {
+            return appDelegate!.memoList.count
+        }else{
+            return -1
+        }
+    }*/
 
     /*
     // Override to support conditional editing of the table view.
