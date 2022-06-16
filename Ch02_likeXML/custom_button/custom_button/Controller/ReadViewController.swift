1d91b25c4092384caf29fd146b409f60902ab647
ysh <happysh_s2@naver.com>
2022-06-16 22:46:57 +0900

    Fix button to present next Scene

--- Ch02_likeXML/custom_button/custom_button/Controller/ReadViewController.swift
+++ Ch02_likeXML/custom_button/custom_button/Controller/ReadViewController.swift
@@ -5,4 +5,22 @@
 //  Created by 양승현 on 2022/06/16.
 //
 
-import Foundation
+import UIKit
+
+class ReadViewController : UIViewController
+{
+    var pEmail    : String?
+    var pUpdate   : Bool?
+    var pInterval : Double?
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        let email    = UILabel()
+        let update   = UILabel()
+        let interval = UILabel()
+    }
+}
