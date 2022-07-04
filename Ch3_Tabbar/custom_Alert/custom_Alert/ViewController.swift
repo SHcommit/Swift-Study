1972558f49dc89c3df5e700e3e7d1e2e77c09fcb
ysh <happysh_s2@naver.com>
2022-07-04 18:55:35 +0900

    Add custom rating alert

--- Ch3_Tabbar/custom_Alert/custom_Alert/ViewController.swift
+++ Ch3_Tabbar/custom_Alert/custom_Alert/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
     lazy var underAlertBtn : UIButton =
@@ -16,20 +17,33 @@ class ViewController: UIViewController {
         alertBtn.center.x = self.view.frame.width / 2
         alertBtn.center.y = self.view.frame.width / 2
         
-        alertBtn.layer.cornerRadius = 3
+        alertBtn.layer.cornerRadius    = 3
         alertBtn.layer.backgroundColor = UIColor(red: 0.4556, green: 0.749, blue: 0.549, alpha: 1).cgColor
-        alertBtn.layer.borderColor = UIColor(red: 0.2, green: 0.3, blue: 0.2, alpha: 0.9).cgColor
+        alertBtn.layer.borderColor     = UIColor(red: 0.2, green: 0.3, blue: 0.2, alpha: 0.9).cgColor
         alertBtn.setTitleColor(UIColor(red: 0, green: 0.498, blue: 0.7749, alpha: 1), for: .normal)
         
         // shadow
         let contactShadowSize = 15
         alertBtn.layer.shadowColor = UIColor.red.cgColor
         
-        alertBtn.layer.shadowRadius = 15
+        alertBtn.layer.shadowRadius  = 15
         alertBtn.layer.shadowOpacity = 0.4
         alertBtn.layer.shadowPath    = CGPath(ellipseIn: CGRect(x: 0, y: 5, width: Int(alertBtn.frame.width), height: Int(alertBtn.frame.height)), transform: nil)
         return alertBtn
     }()
+    
+    lazy var imageAlertBtn : UIButton =
+    {
+        let imageBtn   = UIButton(type: .system)
+        imageBtn.frame = CGRect(x: 0, y: 0, width: 100, height: 30)
+        imageBtn.center.x = self.view.frame.width / 2
+        imageBtn.center.y = self.view.frame.height / 2 - 60
+        
+        imageBtn.layer.cornerRadius = 2
+        imageBtn.setTitle("이미지 alert", for: .normal)
+        return imageBtn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -42,9 +56,11 @@ class ViewController: UIViewController {
         centerAlertBtn.setTitleColor(UIColor(red: 0.5, green: 0.6, blue: 0.3, alpha: 1), for: .normal)
         centerAlertBtn.setTitle("기본 알림창", for: .normal)
         centerAlertBtn.addTarget(self, action: #selector(defaultAlert(_:)), for: .touchUpInside)
-        
+        underAlertBtn.addTarget(self, action: #selector(customAlertShowMap(_:)), for: .touchUpInside)
+        imageAlertBtn.addTarget(self, action: #selector(customAlertShowImage(_:)), for: .touchUpInside)
         self.view.addSubview(centerAlertBtn)
         self.view.addSubview(underAlertBtn)
+        self.view.addSubview(imageAlertBtn)
         
     }
 }
@@ -62,6 +78,24 @@ extension ViewController
         customAlert(&alert)
         self.present(alert, animated: false)
     }
+    @objc func customAlertShowMap(_ sender : Any)
+    {
+        var alert = UIAlertController(title: "여기 맞나요?", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        customAlertShowMapState(&alert)
+        
+        self.present(alert, animated: true)
+    }
+    
+    @objc func customAlertShowImage(_ sender : Any)
+    {
+        var alert = UIAlertController(title : nil,message: "평점은?!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        customAlertShowImageUI(&alert)
+        self.present(alert, animated: true)
+    }
 }
 
 //MARK: - setUI
@@ -73,5 +107,41 @@ extension ViewController
         
         alertCenterView.view.backgroundColor = .red
         alert.setValue(alertCenterView, forKey: "contentViewController")
+    }
+    
+    func customAlertShowMapState(_ alert : inout UIAlertController)
+    {
+        let contentVC     = UIViewController()
+        let mapkitView    = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        contentVC.view    = mapkitView
+        let pos           = CLLocationCoordinate2D(latitude: 37.514322, longitude: 126.894623)
+        let span          = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        let region        = MKCoordinateRegion(center: pos, span: span)
+        mapkitView.region = region
+        mapkitView.regionThatFits(region)
+        
+        let point = MKPointAnnotation()
+        point.coordinate = pos
+        mapkitView.addAnnotation(point)
+        contentVC.preferredContentSize.height = 180
+        alert.setValue(contentVC, forKey: "contentViewController")
+    }
+    
+    func customAlertShowImageUI(_ alert : inout UIAlertController)
+    {
+        let VC = UIViewController()
+        let image = UIImage(named: "rating5")
+        let imgObject = UIImageView(image: image)
+        imgObject.frame = CGRect(x: 0, y: 0, width: (image?.size.width)!, height: (image?.size.height)!)
+        imgObject.layoutMargins.bottom = 10.0
+        //마진주고싶은데 이건 안 되네 VC를 바꿔줘야함
+        //imgObject.layoutMargins = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+        
+        VC.view.addSubview(imgObject)
+        
+        VC.preferredContentSize = CGSize(width: (image?.size.width)!, height: (image?.size.height)! + 10)
+        
+
+        alert.setValue(VC, forKey: "contentViewController")
     }
 }
