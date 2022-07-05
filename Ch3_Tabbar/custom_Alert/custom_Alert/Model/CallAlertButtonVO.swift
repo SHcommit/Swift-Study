b013e54bc73405e1a4e87f668e94cded492af862
ysh <happysh_s2@naver.com>
2022-07-05 23:25:02 +0900

    Add default alert style

---
+++ Ch3_Tabbar/custom_Alert/custom_Alert/Model/CallAlertButtonVO.swift
@@ -0,0 +1,39 @@
+//
+//  CallAlertButtonVO.swift
+//  custom_Alert
+//
+//  Created by 양승현 on 2022/07/05.
+//
+
+import UIKit
+
+class callAlertButtonVO
+{
+    var callAlertButton : UIButton
+    var alert           : UIAlertController
+    init()
+    {
+        callAlertButton = UIButton()
+        alert           = UIAlertController()
+    }
+    //디폴트 버튼
+    func addButtonObject(_ view : inout UIView, _ button : inout UIButton, _ title : String, _ color : UIColor , _ index : Int)
+    {
+        button.frame              = CGRect(x: 0, y: 100, width: 100, height: 30)
+        button.layer.cornerRadius = 2
+        button.backgroundColor    = color
+        button.center.x           = view.frame.width  / 2 
+        button.center.y           = view.frame.height / 10 * CGFloat(index)
+        button.setTitle(title, for: .normal)
+        
+        view.addSubview(button)
+    }
+    //초기 디폴트 알람
+    func defaultAlert(_ title : String?, _ message : String?, _ preferredStyle : UIAlertController.Style ) -> UIAlertController
+    {
+        let alert = UIAlertController(title: title ?? nil, message: message ?? nil, preferredStyle: preferredStyle)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        alert.addAction(UIAlertAction(title: "Cancel", style:.cancel))
+        return alert
+    }
+}
---
+++ Ch3_Tabbar/custom_Alert/custom_Alert/Model/CustomMapAlertVO.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class CustomMapAlert
+{
+    var mapButton : UIButton
+    var mapAlert  : UIAlertController
+    init()
+    {
+        mapButton = UIButton()
+        mapAlert  = UIAlertController()
+    }
+    
+    //MARK: - setUI
+    
+}
