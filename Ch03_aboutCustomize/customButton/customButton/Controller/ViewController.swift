7f01836f8f8fea167d281c55cc51950e3866d8dd
ysh <happysh_s2@naver.com>
2022-07-09 23:33:01 +0900

    update code

--- Ch03_aboutCustomize/customButton/customButton/Controller/ViewController.swift
+++ Ch03_aboutCustomize/customButton/customButton/Controller/ViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     var vcButton = customFirstBtn(frame: CGRect(x: 30, y: 50, width: 150, height: 30), title: "VC에서 추가했지롱")
+    
+    var circleButton = customFirstBtn(type: .circle)
+    var rectBtn      = customFirstBtn(type: .rect)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.addSubview(vcButton)
+        circleButton.frame    = CGRect(x: 30, y: 200, width: 150, height: 30)
+        circleButton.center.y = self.view.center.y / 10 * 4
+        self.view.addSubview(circleButton)
+        
+        rectBtn.frame    = CGRect(x: 30, y: 200, width: 150, height: 30)
+        rectBtn.center.y = self.view.center.y / 10 * 6
+        self.view.addSubview(rectBtn)
+        rectBtn.style = .circle
     }
 
 
--- Ch03_aboutCustomize/customButton/customButton/Model/customFirstBtn.swift
+++ Ch03_aboutCustomize/customButton/customButton/Model/customFirstBtn.swift
@@ -3,6 +3,34 @@ import UIKit
 //오 이렇게 하니까 좋다 굳이 coord 설정 안 해도 되고
 class customFirstBtn : UIButton
 {
+    var style : CSButtonType = .rect
+    {
+        didSet
+        {
+            switch style
+            {
+            case .rect :
+                self.backgroundColor = .cyan
+                self.layer.borderColor = UIColor.cyan.cgColor
+                self.layer.borderWidth = 2
+                self.layer.cornerRadius = 0
+                self.setTitle("rect Btn", for: .normal)
+                self.setTitleColor(.white, for: .normal)
+            case .circle  :
+                self.backgroundColor    = .red
+                self.layer.borderWidth  = 2
+                self.layer.borderColor  = UIColor.red.cgColor
+                self.layer.cornerRadius = 50
+                self.setTitleColor(.white, for: .normal)
+                self.setTitle("circle Btn", for: .normal)
+            }
+        }
+    }
+    init()
+    {
+        super.init(frame: CGRect.zero)
+    }
+    
     //스토리보드에서 쓰일때
     required init(coder aDecorder : NSCoder)
     {
@@ -12,10 +40,36 @@ class customFirstBtn : UIButton
     }
     
     //이건 VC에서도 쓰일 수 있도록.
+    //추가적으로 init(frame:) 를 사용할 때는 이미 부모클래스에 존재해서 override 키워드
+    //써야하는데 지금은 괜찮다. 부모 클래스에 없는 걸 내가 새로 만들었거든 그래도 부모 생성자 함수 호출해줘야해
     init(frame: CGRect, title : String) {
         super.init(frame: frame)
         initialButtonUI(title)
-        self.addTarget(self, action: #selector(btnEvent(_:)), for: .touchUpInside)
+        //self.addTarget(self, action: #selector(clickBtnEvent(_:)), for: .touchUpInside)
+    }
+    
+    
+    convenience init(type: CSButtonType)
+    {
+        self.init()
+        
+        switch type {
+        case .rect:
+            self.backgroundColor    = .blue
+            self.layer.borderColor  = UIColor.blue.cgColor
+            self.layer.borderWidth  = 2
+            self.layer.cornerRadius = 2
+            self.setTitleColor(.white, for: .normal)
+            self.setTitle("rect Btn", for: .normal)
+        case .circle:
+            self.backgroundColor    = .red
+            self.layer.borderWidth  = 2
+            self.layer.borderColor  = UIColor.red.cgColor
+            self.layer.cornerRadius = 50
+            self.setTitleColor(.white, for: .normal)
+            self.setTitle("circle Btn", for: .normal)
+        }
+        self.addTarget(self, action: #selector(clickBtnEvent(_:)), for: .touchUpInside)
     }
 }
 extension customFirstBtn
@@ -32,8 +86,16 @@ extension customFirstBtn
 }
 extension customFirstBtn
 {
-    @objc func btnEvent(_ sender : Any)
+    @objc func clickBtnEvent(_ sender : UIButton)
     {
-        
+        sender.tag += 1
+        sender.setTitle("\(sender.tag) 번째 클릭", for: .normal)
     }
+}
+
+public enum CSButtonType
+{
+    case rect
+    case circle
+    
 }
