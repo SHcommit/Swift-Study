00f543c69200143c23b3a5d91beb45d738ad3b30
ysh <happysh_s2@naver.com>
2022-07-11 03:03:07 +0900

    Add CSStepper object and implement up, down count by event handler

--- Ch03_aboutCustomize/customStepper/customStepper/Controller/ViewController.swift
+++ Ch03_aboutCustomize/customStepper/customStepper/Controller/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var countStepper = CSStepper(frame: CGRect(x: 30, y: 100, width: 130, height: 30))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.addSubview(countStepper)
+        
+        
+        countStepper.stepper.leftBtn.addTarget(self, action: #selector(leftButtonTouchUP(_:)), for: .touchUpInside)
+        countStepper.stepper.rightBtn.addTarget(self, action: #selector(rightButtonTouchUP(_:)), for: .touchUpInside)
     }
-
+    //MARK: - event Handler
+    @objc func leftButtonTouchUP(_ sender : UIButton)
+    {
+        if countStepper.stepper.count != 0
+        {
+            countStepper.stepper.count -= 1
+        }
+        countStepper.stepper.centerLabel.text = String(countStepper.stepper.count)
+    }
+    @objc func rightButtonTouchUP(_ sender : UIButton)
+    {
+        if countStepper.stepper.count != 100
+        {
+            countStepper.stepper.count += 1
+        }
+        countStepper.stepper.centerLabel.text = String(countStepper.stepper.count)
+    }
 
 }
 
