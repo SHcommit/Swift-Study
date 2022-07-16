b08094d7d08416466a2c336e9420a361c8a18e7e
ysh <happysh_s2@naver.com>
2022-07-16 23:02:29 +0900

    Add mvc pattern, resources

---
+++ Ch4_customSideBar/Ch4_customSideBar/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ch4_customSideBar
+//
+//  Created by 양승현 on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
