92b44242832168e9909ba160c8093bc61666123d
ysh <happysh_s2@naver.com>
2022-07-19 01:26:58 +0900

    Add initial func

--- Ch4_customSideBar/Ch4_customSideBar/Controller/RevealViewController.swift
+++ Ch4_customSideBar/Ch4_customSideBar/Controller/RevealViewController.swift
@@ -3,8 +3,18 @@ import UIKit
 /**
  TODO : 프론트 뷰 리어뷰 컨트롤하기 위한 컨트롤러
  
+ # Document: #
+ FrontViewController의 view와 SideBarView의 View를 다루는 뷰다.
+ 컨테이너 뷰라고도 할 수 있다.
+ 
  - Param <#ParamNames#> : <#Discription#>
  - Param <#ParamNames#> : <#Discription#>
+ 
+ - func setUpView : 초기 화면 설정
+ - func getSideView : 사이바 뷰 읽어옴
+ - func setShadowEffect : 콘텐츠 뷰에 그림자 효과
+ - func openSideBar : 사이드 바 열음
+ - func closeSideBar : 사이드 바 닫음
  
  # Notes: #
  1. FrontViewController     : 사용자에게 보여질 첫 화면
@@ -16,5 +26,25 @@ class RevealViewController : UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+    }
+    func setUpView()
+    {
+        
+    }
+    func getSideView()
+    {
+        
+    }
+    func setShadowEffect(_ shadow: Bool, offset: CGFloat)
+    {
+        
+    }
+    func openSideBar(_ complete: (() -> Void)?)
+    {
+        
+    }
+    func closeSideBar(_ complete : (() -> Void)?)
+    {
+        
     }
 }
