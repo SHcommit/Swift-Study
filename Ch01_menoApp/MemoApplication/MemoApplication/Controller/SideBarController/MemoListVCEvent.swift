2545d9b0ee34536225ab40e9d0fe18930e80567d
ysh <happysh_s2@naver.com>
2022-07-23 23:52:18 +0900

    Update @objc func moveSide(_:)

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/MemoListVCEvent.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/MemoListVCEvent.swift
@@ -11,11 +11,23 @@ extension MemoListVC
 {
     @objc func moveSide(_ sender : Any)
     {
-        if self.sideBarDelegate?.isSideBarShowing == false
+        
+        if sender is UIBarButtonItem
+        {
+            if self.sideBarDelegate?.isSideBarShowing == false
+            {
+                self.sideBarDelegate?.openSideBar(nil)
+            }
+            else
+            {
+                self.sideBarDelegate?.closeSideBar(nil)
+            }
+        }
+        else if sender is UIScreenEdgePanGestureRecognizer
         {
             self.sideBarDelegate?.openSideBar(nil)
         }
-        else
+        else if sender is UISwipeGestureRecognizer
         {
             self.sideBarDelegate?.closeSideBar(nil)
         }
