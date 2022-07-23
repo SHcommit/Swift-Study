68a65a7a5402b9c73b9fc89309335254f318cfe8
ysh <happysh_s2@naver.com>
2022-07-24 02:38:29 +0900

    Add proflie in sidebar's tableHeader

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/SideBarViewContorller.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/SideBarViewContorller.swift
@@ -6,37 +6,51 @@
 //
 
 import UIKit
-// 사용자의 panning이나 메뉴 클릭시 테이블 뷰가 호출되도록!!! 해야함
+
+/**
+ TODO : 사용자의 panning 제스쳐나 메뉴 클릭 시 SideVC의 view에 테이블 뷰가 호출되야함.
+ 
+ - Param list : 사이드 바 목록
+ - Param profile : 사이드 바에서 정의된 user's profile
+ 
+ # Notes: #
+ 1. <#Notes if any#>
+ 
+ */
 class SideBarViewController : UITableViewController
 {
+    let list    = SideBarListDTO()
+    var profile = SideBarProfileVO()
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        tableHeaderSetup()
     }
     
     override func tableView(_ tableView : UITableView, numberOfRowsInSection section : Int) -> Int
     {
-        return 0
+        return list.titles.count
     }
     override func tableView(_ tableView : UITableView, cellForRowAt indexPath : IndexPath) -> UITableViewCell
     {
-        let id = "SideBarMenuCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: id) ?? UITableViewCell(style: .default ,reuseIdentifier: id)
+        let id    = "SideBarMenuCell"
+        let cell  = tableView.dequeueReusableCell(withIdentifier:"id") ?? UITableViewCell(style:.default,reuseIdentifier: id)
+        let title = list.titles[indexPath.row]
+        let icon  = list.icons[indexPath.row]
         
+        cell.imageView?.image  = icon
+        cell.textLabel?.text   = title
+        cell.textLabel?.font   = UIFont.systemFont(ofSize:14)
         
         return cell
     }
     
-    func tableHeaderUI()
+    func tableHeaderSetup()
     {
-        let accountLabel   = UILabel()
-        accountLabel.frame = CGRect(x: 10, y: 30, width: self.view.frame.width, height: 30)
-        
-        let contentView   = UIView()
-        contentView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 90)
-        contentView.backgroundColor = .brown
-        contentView.addSubview(accountLabel)
-        self.tableView.tableHeaderView = contentView
+        profile.headerViewSetup(self.tableView)
+        profile.nameLabelSetup( "ios새싹 승현이")
+        profile.emailLabelSetup( "happysh_s2@naver.com")
+        profile.profileImageSetup( "account.jpg")
     }
 }
 
