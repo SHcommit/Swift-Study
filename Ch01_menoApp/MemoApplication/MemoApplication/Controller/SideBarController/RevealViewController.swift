01d95424c02dfb567167d9727a8b97b0af8317ab
ysh <happysh_s2@naver.com>
2022-07-24 01:10:02 +0900

    Add code review.

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/RevealViewController.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/RevealViewController.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+/**
+ TODO : MemoListVC와 SideBarVC를 다루는 Controller
+ 
+ - Param frontVC : MemoListVC instance
+ - Param sideVC : SideBarViewController instance
+ - Param isSideBarShowing : SideBar's instance의 value or nil
+ - Param (SLIDE_TIEM,SLIDEBAR_WIDTH) : (화면 열리는 ainmation시간 ,열릴 x 위치)
+ 
+ # Notes: #
+ 1. setUpFrontView()
+    frontVC인스턴스를 RevealVC의 자식으로 등록한다.
+    frontVC의 대리자에게 위임한다.
+ 2. getSideView()
+    sideVC 인스턴스를 RevealVC의 자식으로 등록한다.
+    frontVC가 초기 화면에 보여짐으로 bringSubviewToFront()를 적용한다.
+ 3. setShadowEffect(_: offset:)
+    frontVC의 layout에 shadow를 적용시킨다.
+ 4. openSideBar(_:)
+    사용자의 barButton클릭이나 panning제스쳐 동작 시 SideVC의 인스턴스를 생성하고 animation을 통해 보여진다.
+ 5. closeSideBar(_:)
+    사용자의 barBtn 클릭이나 swipe 제스쳐 동작 시 SideVC의 인스턴스 해제랑 frontVC의 layer shadow를 해제한다.
+ */
 class RevealViewController : UIViewController
 {
     var frontVC : UIViewController?
@@ -26,14 +48,12 @@ class RevealViewController : UIViewController
             NSLog("Reveal's FrontVC value == nil")
             return
         }
-        self.frontVC = vc
-        
+        self.frontVC            = vc
+        let frontViewController = vc.viewControllers[0] as? MemoListVC
         self.addChild(vc)
         self.view.addSubview(vc.view)
         vc.didMove(toParent: self)
         
-        //대리자한테 위임함. by using getSideView()..
-        let frontViewController = vc.viewControllers[0] as? MemoListVC
         frontViewController?.sideBarDelegate = self
     }
     func getSideView()
@@ -51,6 +71,7 @@ class RevealViewController : UIViewController
         self.addChild(vc)
         self.view.addSubview(vc.view)
         vc.didMove(toParent:self)
+        
         guard let frontViewController = self.frontVC else
         {
             NSLog("Reveal's FrontVC == nil.\n check setUpFrontView()")
@@ -71,8 +92,8 @@ class RevealViewController : UIViewController
         }
         else
         {
-            self.frontVC?.view.layer.cornerRadius = 0.0
-            self.frontVC?.view.layer.shadowOffset = CGSize(width:0,height:0)
+            self.frontVC?.view.layer.cornerRadius  = 0.0
+            self.frontVC?.view.layer.shadowOffset  = CGSize(width:0,height:0)
         }
     }
     func openSideBar(_ complete: (()->Void)?)
@@ -87,14 +108,13 @@ class RevealViewController : UIViewController
             options: options,
             animations:
             {
-                //frontVC는 뷰 컨트롤러니까.
                 self.frontVC?.view.frame = CGRect(x: self.SIDEBAR_WIDTH, y: 0, width: Int(self.view.frame.width), height: Int(self.view.frame.height))
             },
             completion:
             {
+                //성공적으로 animation이 실행됬는가?
                 if $0
                 {
-                    //에니메이션 실행이 됬는가?
                     self.isSideBarShowing = true
                     complete?()
                 }
@@ -114,7 +134,6 @@ class RevealViewController : UIViewController
             },
             completion:
             {
-                //성공적으로 닫혔는가?
                 if $0
                 {
                     self.sideVC?.view.removeFromSuperview()
