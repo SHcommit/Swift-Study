9a6ed3030993b977c81f040fa1d7d05fb56baa33
ysh <happysh_s2@naver.com>
2022-07-30 04:30:11 +0900

    Commit for pull README.md data...

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/Cells/six/ProfileVC.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/Cells/six/ProfileVC.swift
@@ -1,8 +1,19 @@
-//
-//  ProfileVC.swift
-//  MemoApplication
-//
-//  Created by 양승현 on 2022/07/30.
-//
+import UIKit
 
-import Foundation
+/**
+ TODO : 사이드 바 상단의 프로필 영역 detail Scene. 사이드 바 메뉴 6번째 Cell인 계정관리와 연결됨.
+       계정 정보나 입력, 수정도 가능하다.
+  
+ - Param <#ParamNames#> : <#Discription#>
+ - Param <#ParamNames#> : <#Discription#>
+ 
+ # Notes: #
+ 1. <#Notes if any#>
+ 
+ */
+class ProfileVC : UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
