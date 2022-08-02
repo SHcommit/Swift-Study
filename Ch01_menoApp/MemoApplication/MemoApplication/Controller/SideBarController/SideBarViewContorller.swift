2b4065f409e6c9508d09676ae5077d26a2b24d79
ysh <happysh_s2@naver.com>
2022-08-02 15:54:20 +0900

    Fix profileVC 호출시 네비게이션 상속 안되는 문제 해결.

--- Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/SideBarViewContorller.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Controller/SideBarController/SideBarViewContorller.swift
@@ -19,8 +19,13 @@ import UIKit
     ----해결----
     FrontVC의 인스턴스는 RevealVC로 다룰 수있기에 해당 인스턴스를 얻어온 후에 FrontVC의 segue로 연결된 UINavigationController로 캐스팅 후 성공적으로 memoFormVC를 얻어왔다.
     ----문제----
-    근데 지금 SideVC가 닫히지 않고  FrontVC의 자리에서 memoForm만 호출된다.. ㅋㅋ 여전히 sideVC는 남아있다.
- 
+    근데 지금 SideVC가 닫히지 않고  FrontVC의 자리에서 memoForm만 호출된다.. ㅋㅋ 여전히 sideVC는 남아있다.-> 해결
+ 2. Date : 22.08.02
+    스토리보드에서 profileVC와 rootviewController 로 navigationController가 있는데 profileVC의 storyboard 인스턴스를 얻었는데 화면 전환으로 해당 뷰를 호출 했을 때 rootView의 컨트롤러가 적용이 안된다.
+    profileVC의 내용물은 보이는데 navigationController가 적용이 안되는 상황.
+    ----해결----
+    rootViewController(navigaitonController)는 독립적으로 뭔가 표시할 수없기에 profileVC를 루트뷰로 등록한것.
+    따라서 rootViewController의 스토리보드id를 인스턴스로 생성한 후 호출하면 성공적으로 navigationController 까지 적용이 된다.
  */
 class SideBarViewController : UITableViewController
 {
@@ -64,6 +69,19 @@ class SideBarViewController : UITableViewController
             revealVC?.closeSideBar
             {
                 target.pushViewController(memoFormVC, animated: true)
+            }
+            break;
+        case 5:
+            
+            guard let uv = self.storyboard?.instantiateViewController(withIdentifier:"_profile") else
+            {
+                return NSLog("_profile Scene can't find in SideBarViewController.swift")
+            }
+            //이걸 하지 않으면 일반적인 present시에 나타나는 화면 전환으로 됨 stack느낌으로 이전 view가 뒤에 존재함.
+            uv.modalPresentationStyle = .fullScreen
+            self.present(uv, animated: true)
+            {
+                self.revealVC?.closeSideBar(nil)
             }
             break;
         default:
