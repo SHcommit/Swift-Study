4caa7133fe222ddbeabb974ae8137ab8e613e4cc
ysh <happysh_s2@naver.com>
2022-08-03 14:04:40 +0900

    Create UserDefaults

---
+++ Ch05_dataStorage/UserDefaults/UserDefaults/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UserDefaults
+//
+//  Created by 양승현 on 2022/08/03.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
