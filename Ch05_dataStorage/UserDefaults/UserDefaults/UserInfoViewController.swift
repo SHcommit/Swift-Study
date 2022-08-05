91111d9b41883955b3f0dd6a18911a75760c2096
ysh <happysh_s2@naver.com>
2022-08-06 06:33:49 +0900

    Add UIView, NSLayoutConstraint comment

--- Ch05_dataStorage/UserDefaults/UserDefaults/UserInfoViewController.swift
+++ Ch05_dataStorage/UserDefaults/UserDefaults/UserInfoViewController.swift
@@ -123,47 +123,38 @@ class UserInfoViewController : UITableViewController
         switch indexPath.row
         {
         case 0:
-            let alert = UIAlertController(title:"input user's name :)", message: nil, preferredStyle: .alert)
+            let alert = UIAlertController(title:"사용자님의 이름을 입력해주세요.", message: nil, preferredStyle: .alert)
             contents = UIViewController()
             guard let centerVC = contents else
             {
                 return NSLog("contents instance is nil in UserInfoViewController.swift")
             }
-//            let tf           = UITextField()
-//            tf.frame         = CGRect(x: 0, y: 0, width: alert.view.frame.width / 2, height: 30)
-//            tf.text          = self.name.text
-//            tf.textAlignment = .center
-//            tf.backgroundColor    = .white
-//            tf.layer.cornerRadius = 3
-//            tf.layer.borderColor  = UIColor.lightGray.cgColor
-//            tf.layer.borderWidth  = 0.3
-//            centerVC.view.addSubview(tf)
-//            centerVC.preferredContentSize = CGSize(width: tf.frame.width, height: tf.frame.height)
-//            alert.view.addSubview(centerVC.view)
-            
-//            alert.setValue(centerVC, forKey: "contentViewController")
-            let centerView = UIView()
-            let tf = UITextField()
-            tf.frame = CGRect(x:0,y:0,width:alert.view.frame.width/2,height:30)
-           
+            //let cv = UIView()
+
+            let tf           = UITextField()
+            tf.frame         = CGRect(x: 0, y: 0, width: alert.view.frame.width / 2, height: 30)
             tf.text          = self.name.text
-            centerView.addSubview(tf)
-            centerView.leftAnchor.constraint(equalTo: alert.view.leftAnchor).isActive = true
-            centerView.rightAnchor.constraint(equalTo: alert.view.rightAnchor).isActive = true
-            centerView.heightAnchor.constraint(equalTo: alert.view.heightAnchor).isActive = true
+            tf.textAlignment = .center
+            tf.backgroundColor    = .white
+            tf.layer.cornerRadius = 3
+            tf.layer.borderColor  = UIColor.lightGray.cgColor
+            tf.layer.borderWidth  = 0.3
+            centerVC.view.addSubview(tf)
+            centerVC.preferredContentSize = CGSize(width: tf.frame.width, height: tf.frame.height)
+            alert.view.addSubview(centerVC.view)
+            /*
+             알림창에 뷰 컨트롤러 등록
+             */
+            alert.setValue(centerVC, forKey: "contentViewController")
             
-            centerView.bottomAnchor.constraint(equalTo:alert.view.bottomAnchor).isActive = true
-            NSLayoutConstraint.activate(
-                [
-                    centerView.leftAnchor.constraint(equalTo: alert.view.leftAnchor),
-                    centerView.rightAnchor.constraint(equalTo: alert.view.rightAnchor),
-                    centerView.heightAnchor.constraint(equalTo: alert.view.heightAnchor),
-                    
-                    centerView.bottomAnchor.constraint(equalTo:alert.view.bottomAnchor)])
-            
-            
-            
-            alert.view.addSubview(centerView)
+            //UIView로 컨텐츠 뷰를 시도했으나 좌표가 달라져서 패스!!
+//            cv.addSubview(tf)
+//            alert.view.addSubview(cv)
+//            NSLayoutConstraint.activate([
+//                
+//                cv.bottomAnchor.constraint(equalTo: alert.view.bottomAnchor, constant: -200),
+//                cv.heightAnchor.constraint(equalToConstant: 100)
+//            ])
             alert.addAction(
                 UIAlertAction(title: "OK", style: .default)
                 { (_) in
