8a51c685d64d5215356b5e3497f055c48a503cfb
ysh <happysh_s2@naver.com>
2022-08-11 00:38:58 +0900

    Add customizing alertVO calss

--- Ch01_menoApp/MemoApplication/MemoApplication/Model/defaultAlert.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Model/defaultAlert.swift
@@ -1,8 +1,23 @@
-//
-//  defaultAlert.swift
-//  MemoApplication
-//
-//  Created by 양승현 on 2022/08/10.
-//
+import UIKit
 
-import Foundation
+class customAlertVO
+{
+    var alert      : UIAlertController!
+    var centerView : UIViewController!
+    
+    init(title: String?, message : String?, style: UIAlertController.Style)
+    {
+        alert = UIAlertController(title: title, message: message, preferredStyle: style)
+    }
+    func addBtn(title : String, style : UIAlertAction.Style, completion : ((UIAlertAction) -> Void)?)
+    {
+        let btn = UIAlertAction(title: title, style: style, handler: completion)
+        alert.addAction(btn)
+    }
+    func addCenterVC()
+    {
+        centerView = UIViewController()
+        alert.view.addSubview(centerView.view)
+        alert.setValue(centerView,forKey: "contentViewController")
+    }
+}
