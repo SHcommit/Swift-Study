f0f133a05684efc894ddab93aafd9f116ea1c75e
ysh <happysh_s2@naver.com>
2022-08-10 22:57:30 +0900

    Add code review about UserInfoManager class

--- Ch01_menoApp/MemoApplication/MemoApplication/UserInfo/UserInfoManager.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/UserInfo/UserInfoManager.swift
@@ -1,5 +1,22 @@
 import UIKit
 
+/**
+ TODO : UserDefaults.standard를 통해 User의 정보를 관리하는 클래스.
+ 
+ - Param plist : UserDefaults.standard
+ - Param loginID : UserDefaults.standard에 id값을 부여해서 저장하거나 꺼내옴
+ - Param account : plist에 account를 저장하거나 꺼내옴
+ - Param name : plist에 이름을 저장하거나 꺼내온다.
+ - Param profile : plist에 data형식으로 UIImage를 저장하거나 꺼내온다.
+ - Param isLogin : plist의 UserInfoKeyDTO.loginID 값이나 UserInfoKeyDTO.account값이 존재한다면 로그인됨.
+ 
+ - login(_:_: ) func의 첫번째, 두번째 매개변수를 통해 사용자 기록한 로그인 정보가 맞는 정보인지 체크
+ - logout() plist에 저장됬던 UserInfoKeyDTO 키값들 전부 삭제.
+ 
+ # Notes: #
+ 1. <#Notes if any#>
+ 
+ */
 class UserInfoManager
 {
     var plist   = UserDefaults.standard
