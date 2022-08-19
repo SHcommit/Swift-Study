a9f313e5291f3603c267823aedc5f810147adeb8
ysh <happysh_s2@naver.com>
2022-08-19 15:06:35 +0900

    Add custom alert

--- Ch06_SQLite3/HRApp/HRApp/Model/CustomAlert.swift
+++ Ch06_SQLite3/HRApp/HRApp/Model/CustomAlert.swift
@@ -1,8 +1,39 @@
-//
-//  customAlert.swift
-//  HRApp
-//
-//  Created by 양승현 on 2022/08/19.
-//
+import UIKit
 
-import Foundation
+class CustomAlert
+{
+    enum arrow
+    {
+        case left , right
+    }
+    var alert    : UIAlertController?
+    var leftBtn  : UIAlertAction?
+    var rightBtn : UIAlertAction?
+    var contentVC : UIViewController?
+    func setupAlert(_ title : String?, _ message: String? , _ style : UIAlertController.Style)
+    {
+        alert = UIAlertController(title: title, message: message, preferredStyle: style)
+    }
+    func setupContent()
+    {
+        let vc = UIViewController()
+        contentVC = vc
+        alert?.view.addSubview(vc.view)
+        alert?.setValue(vc, forKey: "contentViewController")
+    }
+    func setupBtn(_ title : String?, _ style: UIAlertAction.Style,_ arrow: arrow, _ handler : ((UIAlertAction)->())?)
+    {
+        switch arrow
+        {
+        case .left:
+            leftBtn = UIAlertAction(title: title, style: style, handler: handler)
+            break;
+        case .right:
+            rightBtn = UIAlertAction(title: title, style: style, handler: handler)
+            break;
+        default:
+            ()
+            break;
+        }
+    }
+}
