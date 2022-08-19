1d2a373ee7687f09fc50f2243a38fc02177a1a60
ysh <happysh_s2@naver.com>
2022-08-19 15:05:52 +0900

    Create file

---
+++ Ch06_SQLite3/HRApp/HRApp/Model/CustomAlert.swift
@@ -0,0 +1,8 @@
+//
+//  customAlert.swift
+//  HRApp
+//
+//  Created by 양승현 on 2022/08/19.
+//
+
+import Foundation
---
+++ Ch06_SQLite3/HRApp/HRApp/Model/NaviBtnVO.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class DepartNaviBtnVO
+{
+    var EditBtn : UINavigationItem?
+    var AddBtn  : UINavigationItem?
+    var 
+    init()
+    {
+        
+    }
+    func setupEditBtn()
+    {
+        
+    }
+}
