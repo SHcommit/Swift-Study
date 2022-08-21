cc4a0b87eee9ac247e68d50e1445effe20ca7d65
ysh <happysh_s2@naver.com>
2022-08-22 01:49:09 +0900

    Connect db to tableView

--- Ch06_SQLite3/HRApp/HRApp/Controller/Department/DepartmentViewController.swift
+++ Ch06_SQLite3/HRApp/HRApp/Controller/Department/DepartmentViewController.swift
@@ -6,32 +6,36 @@ import UIKit
  - Param <#ParamNames#> : <#Discription#>
  
  # Notes: #
- 1. <#Notes if any#>
+ 1. 앗ㅋㅋㅋ list edit모드 버튼 직접 구현했는데 원래 left바버튼아이템 = self.editButtonItem으로하면 편집기능을 지원한다..
  
  */
 class DepartmentViewController : UITableViewController
 {
-    var arr = ["1","2","3","4"]
-    var naviBtnVO = NaviBtnVO()
+    //var arr        = ["1","2","3","4"]
+    var departList : [DepartRecord]!
+    let departDAO  = DepartmentDAO()
+    var naviBtnVO  = NaviBtnVO()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        naviBtnVO.setupEditBtn(self, #selector(editDepartInfo(_:)))
-        self.navigationItem.leftBarButtonItem = naviBtnVO.EditBtn
-        naviBtnVO.setupAddBtn(self, #selector(addDepartInfo(_:)))
-        self.navigationItem.rightBarButtonItem = naviBtnVO.AddBtn
+        //db에서 정보 가져온다.
+        self.departList = self.departDAO.find()
+        initUI()
     }
     
     //MARK: - tableViewDelegate
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section : Int) -> Int
     {
-        return arr.count
+        return self.departList.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath : IndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCell(withIdentifier:"departCell") ?? UITableViewCell(style: .default, reuseIdentifier: "departCell")
-        cell.textLabel?.text = arr[indexPath.row]
-        //cell.detailTextLabel?.text // aanj2
+        let cell             = tableView.dequeueReusableCell(withIdentifier:"departCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "departCell")
+        cell.accessoryType   = .disclosureIndicator
+        cell.textLabel?.text = self.departList[indexPath.row].departTitle
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        cell.detailTextLabel?.text = self.departList[indexPath.row].departAddr
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 12)
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -45,9 +49,33 @@ class DepartmentViewController : UITableViewController
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete
         {
-            arr.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            let departCd = self.departList[indexPath.row].departCd
+            if departDAO.remove(cd: departCd)
+            {
+                departList.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
         }
+    }
+    
+    //MARK: - setupUI
+    func initUI()
+    {
+        let navTitle = UILabel(frame:CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 14)
+        navTitle.text = "부서 목록 \n" + " 총 \(self.departList?.count ?? 0) 개"
+        
+        self.navigationItem.titleView = navTitle
+        
+        naviBtnVO.setupEditBtn(self, #selector(editDepartInfo(_:)))
+        self.navigationItem.leftBarButtonItem = naviBtnVO.EditBtn
+        naviBtnVO.setupAddBtn(self, #selector(addDepartInfo(_:)))
+        self.navigationItem.rightBarButtonItem = naviBtnVO.AddBtn
+        
+        //셀 스와이프 시 편집모드
+        self.tableView.allowsSelectionDuringEditing = true
     }
 }
 
@@ -67,16 +95,33 @@ extension DepartmentViewController
     {
         var alert = CustomAlert()
         alert.setupAlert("신규 부서 등록", "신규 부서를 등록해주세요", .alert)
-        alert.alert?.addTextField()
-        alert.alert?.addTextField()
-        alert.alert?.textFields?[0].placeholder = "부서명"
-        alert.alert?.textFields?[1].placeholder = "주소"
+        alert.alert?.addTextField() { $0.placeholder = "부서명" }
+        alert.alert?.addTextField() { $0.placeholder = "주소" }
+        
         alert.setupBtn("취소", .cancel,.left, nil)
         alert.setupBtn("확인", .default,.right)
         {
             _ in
-            //여기에 이제 부소를 거기따주는거지 디비에따가
-            self.arr.append((alert.alert?.textFields?[0].text)! )
+            
+            guard let title = alert.alert.textFields?[0].text , let addr = alert.alert.textFields?[1].text else
+            {
+                NSLog("사용자의 입력 잘못됨")
+                return
+            }
+            
+            if self.departDAO.create(title: title, addr: addr)
+            {
+                self.departList = self.departDAO.find()
+                self.tableView.reloadData()
+                
+                let navTitle = self.navigationItem.titleView as? UILabel
+                guard let _navTitle = navTitle else
+                {
+                    NSLog("네비타이틀 캐스팅 안됨")
+                    return
+                }
+                _navTitle.text = "부서 목록\n" + "총 \(self.departList.count) 개"
+            }
             self.tableView.reloadData()
         }
         self.present(alert.alert, animated: true )
