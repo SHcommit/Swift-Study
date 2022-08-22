d7ecd2599323aaa18dc674dcdd878b2f429ed083
ysh <happysh_s2@naver.com>
2022-08-22 15:43:32 +0900

    Add pickerView in CustemAlert

--- Ch06_SQLite3/HRApp/HRApp/Controller/Employee/DepartAlertPicker.swift
+++ Ch06_SQLite3/HRApp/HRApp/Controller/Employee/DepartAlertPicker.swift
@@ -1,8 +1,33 @@
-//
-//  departAlertPicker.swift
-//  HRApp
-//
-//  Created by 양승현 on 2022/08/22.
-//
+import UIKit
 
-import Foundation
+class departPicker : UIPickerView , UIPickerViewDelegate, UIPickerViewDataSource
+{
+    var _list : [DepartRecord]! = [DepartRecord]()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    convenience init(list : [DepartRecord]!, frame : CGRect)
+    {
+        self.init(frame: frame)
+        _list = list
+        self.delegate = self
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self._list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let (_, title, addr) = _list[row]
+        return title+"("+addr+"호)"
+    }
+}
