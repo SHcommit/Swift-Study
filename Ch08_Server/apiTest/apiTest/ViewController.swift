3e0ecd6e550cda66b86a71ff98d8c8795f01db92
ysh <happysh_s2@naver.com>
2022-08-26 19:20:27 +0900

    기본적인 url 호출 방법

--- Ch08_Server/apiTest/apiTest/ViewController.swift
+++ Ch08_Server/apiTest/apiTest/ViewController.swift
@@ -13,7 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    let urlPath = "http://swiftapi.rubypaper.co.kr:2029/practice/currentTime"
+    
+    @IBOutlet weak var currentTime: UILabel!
+    @IBAction func callCurrentTime(_ sender: Any)
+    {
+        do
+        {
+            guard let url = URL(string: urlPath) else {return}
+            let response = try String(contentsOf: url)
+            self.currentTime.text = response
+            self.currentTime.sizeToFit()
+        }
+        catch let err as NSError
+        {
+            NSLog(err.localizedDescription)
+        }
+    }
+    
 }
 
