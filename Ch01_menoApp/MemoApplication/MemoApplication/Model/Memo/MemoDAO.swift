398e243a3b3d4271407725f7364d87f1dd9756d7
ysh <happysh_s2@naver.com>
2022-09-29 12:41:18 +0900

    Refactor

--- Ch01_menoApp/MemoApplication/MemoApplication/Model/Memo/MemoDAO.swift
+++ Ch01_menoApp/MemoApplication/MemoApplication/Model/Memo/MemoDAO.swift
@@ -7,16 +7,13 @@
 import UIKit
 import CoreData
 
-class MemoDAO
-{
-    lazy var context: NSManagedObjectContext =
-    {
+class MemoDAO {
+    lazy var context: NSManagedObjectContext = {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.persistentContainer.viewContext
     }()
     
-    func fetch(keyword text : String? = nil)->[MemoData]
-    {
+    func fetch(keyword text : String? = nil)->[MemoData] {
         var memoList = [MemoData]()
         //MemoMO 요청 객체
         //fetchrequest()두개가 있어서 타입 어노데이션을 명시해줘야함.
@@ -27,16 +24,13 @@ class MemoDAO
         fetchRequest.sortDescriptors = [regDateDesc]
         
         //NSPredicate검색 조건 추가
-        if let t = text, t.isEmpty == false
-        {
+        if let t = text, t.isEmpty == false {
             fetchRequest.predicate = NSPredicate(format: "contents CONTAINS[c] %@", t)
         }
         
-        do
-        {
+        do {
             let resultest = try self.context.fetch(fetchRequest)
-            for rec in resultest
-            {
+            for rec in resultest {
                 let data      = MemoData()
                 data.title    = rec.title
                 data.contents = rec.contents
@@ -44,32 +38,27 @@ class MemoDAO
                 data.regdate  = _regdate
                 data.objectID = rec.objectID
                 
-                if let image = rec.image as Data?
-                {
+                if let image = rec.image as Data? {
                     data.image = UIImage(data: image)
                 }
                 memoList.append(data)
             }
-        }
-        catch let err as NSError
-        {
+        } catch let err as NSError {
             NSLog("An error has occured : %s", err.localizedDescription)
         }
         return memoList
     }
-    func insert(_ data: MemoData)
-    {
+    
+    func insert(_ data: MemoData) {
         guard let obj = NSEntityDescription.insertNewObject(forEntityName: "Memo", into: self.context) as? MemoMO else {NSLog("MemoDAO insert(_:) entity nil");return}
         obj.title     = data.title
         obj.contents  = data.contents
         guard let _regdate = data.regdate else {NSLog("MemoDAO data.regdate is nil");return}
         obj.regdate   = _regdate
-        if let image  = data.image, let _pngData = image.pngData()
-        {
+        if let image  = data.image, let _pngData = image.pngData() {
             obj.image = _pngData
         }
-        do
-        {
+        do {
             //컨텍스트를 통해 영구 저장소에 반영
             try self.context.save()
             //로그인 되어있을 경우 서버에 업로드 기기
@@ -81,24 +70,19 @@ class MemoDAO
                     sync.uploadDatum(obj)
                 }
             }
-        }
-        catch let e as NSError
-        {
+        } catch let e as NSError {
             NSLog("An error has occured : %s", e.localizedDescription)
         }
     }
-    func delete(_ objectID : NSManagedObjectID) -> Bool
-    {
+    
+    func delete(_ objectID : NSManagedObjectID) -> Bool {
         let obj = self.context.object(with: objectID)
         self.context.delete(obj)
         
-        do
-        {
+        do {
             try self.context.save()
             return true
-        }
-        catch let e as NSError
-        {
+        }catch let e as NSError {
             NSLog("An error has occured : %s",e.localizedDescription)
             return false
         }
