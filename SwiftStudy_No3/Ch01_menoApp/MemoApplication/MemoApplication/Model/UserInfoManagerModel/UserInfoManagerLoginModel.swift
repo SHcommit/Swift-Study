12c2307346061c3ab803ffccc88c6b737be5ab53
ysh <happysh_s2@naver.com>
2022-09-29 14:15:22 +0900

    Success convert Codable type to decode parsing data :)

--- SwiftStudy_No3/Ch01_menoApp/MemoApplication/MemoApplication/Model/UserInfoManagerModel/UserInfoManagerLoginModel.swift
+++ SwiftStudy_No3/Ch01_menoApp/MemoApplication/MemoApplication/Model/UserInfoManagerModel/UserInfoManagerLoginModel.swift
@@ -4,14 +4,14 @@ struct UserInfoManagerLoginModel: Codable {
     let resultCode: Int
     let accessToken: String
     let refreshToken: String
-    //let userInfo: UserInfoModel
+    let userInfo: UserInfoModel
     let errorMessage: String
     
     enum CodingKeys: String, CodingKey {
         case resultCode = "result_code"
         case accessToken = "access_token"
         case refreshToken = "refresh_token"
-        
+        case userInfo = "user_info"
         case errorMessage = "error_msg"
     }
 }
@@ -20,7 +20,7 @@ struct UserInfoModel: Codable {
     let loginID: Int
     let account: String
     let name: String
-    let profile: profileImage
+    let profile: Data?
     
     enum CodingKeys: String, CodingKey {
         case loginID = "user_id"
@@ -29,32 +29,43 @@ struct UserInfoModel: Codable {
         case profile = "profile_path"
     }
     
-    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.loginID = try container.decode(Int.self, forKey: .loginID)
-        self.account = try container.decode(String.self, forKey: .account)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.profile = try container.decodeIfPresent(profileImage.self, forKey: .profile) ?? profileImage(imgPath: nil)
+        loginID = try container.decode(Int.self, forKey: .loginID)
+        account = try container.decode(String.self, forKey: .account)
+        name = try container.decode(String.self, forKey: .name)
         
+        guard let imagePath = try? container.decode(String.self, forKey: .profile),
+            let imgURL = URL(string: imagePath),
+            let imgData = try? Data(contentsOf: imgURL) else {
+            profile = nil
+            return
+        }
+        
+        profile = imgData
     }
 }
 
-struct profileImage: Codable {
-    public let imgData: Data?
-    
-    init(imgPath: String?) {
-        guard let imgPath = imgPath else {
-            imgData = nil
-            return
-        }
-        if let imgData = try? Data(contentsOf: URL(string: imgPath)!) {
-            guard let data = UIImage(data: imgData)?.pngData() else {
-                fatalError()
-            }
-            self.imgData = data
-            return
-        }
-        imgData = nil
-    }
-}
+//struct profileImage: Codable {
+//
+//    public let imgData: Data?
+//
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        let imagePath = try container.decodeIfPresent(String.self, forKey: .imgData) ?? nil
+//        guard let imagePath = imagePath, let imgURL = URL(string: imagePath) else {
+//            imgData = nil
+//            return
+//        }
+//
+//        guard let imgData = try? Data(contentsOf: imgURL) else {
+//            self.imgData = nil
+//            return
+//        }
+//        self.imgData = imgData
+//    }
+//
+//    init() {
+//        imgData = nil
+//    }
+//}
